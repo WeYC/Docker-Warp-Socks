6f388ef643151e30ab6e48ee0393ce05954490c2
WeYC <weovo@hotmail.com>
2024-01-05 22:52:27 +0800

    Remove redundant code

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,16 +5,21 @@ IFACE=$(ip route show default | awk '{print $5}')
 IPv4=$(ifconfig "$IFACE" | awk '/inet /{print $2}' | cut -d' ' -f2)
 IPv6=$(ifconfig "$IFACE" | awk '/inet6 /{print $2}' | cut -d' ' -f2)
 
-if [ ! -e "/opt/wgcf/wgcf-profile.conf" ]; then
-
-    wgcf register --accept-tos && wgcf generate && mv wgcf-profile.conf /opt/wgcf
-    sed -i "/\[Interface\]/a PostDown = ip -6 rule delete from ${IPv6}  lookup main" /opt/wgcf/wgcf-profile.conf
-    sed -i "/\[Interface\]/a PostUp = ip -6 rule add from ${IPv6} lookup main" /opt/wgcf/wgcf-profile.conf
-    sed -i "/\[Interface\]/a PostDown = ip -4 rule delete from ${IPv4} lookup main" /opt/wgcf/wgcf-profile.conf
-    sed -i "/\[Interface\]/a PostUp = ip -4 rule add from ${IPv4} lookup main" /opt/wgcf/wgcf-profile.conf
+if [ ! -e "/opt/wgcf/wgcf-account.toml" ]; then
+    wgcf register --accept-tos
 fi
 
+if [ ! -e "/opt/wgcf/wgcf-profile.conf"]; then
+    wgcf generate
+fi
+
+sed -i "/\[Interface\]/a PostDown = ip -6 rule delete from ${IPv6}  lookup main" /opt/wgcf/wgcf-profile.conf
+sed -i "/\[Interface\]/a PostUp = ip -6 rule add from ${IPv6} lookup main" /opt/wgcf/wgcf-profile.conf
+sed -i "/\[Interface\]/a PostDown = ip -4 rule delete from ${IPv4} lookup main" /opt/wgcf/wgcf-profile.conf
+sed -i "/\[Interface\]/a PostUp = ip -4 rule add from ${IPv4} lookup main" /opt/wgcf/wgcf-profile.conf
+
 /bin/cp -rf /opt/wgcf/wgcf-profile.conf /etc/wireguard/warp.conf
+
 wg-quick up warp
 
 exec "$@"
