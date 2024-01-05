d51b2e0ad26a014f0a6693d35cf08911d6f7a446
WeYC <weovo@hotmail.com>
2024-01-05 22:18:23 +0800

    update entry

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,11 @@
 set -e
 
 IFACE=$(ip route show default | awk '{print $5}')
+IPv4=$(ifconfig "$IFACE" | awk '/inet /{print $2}' | cut -d' ' -f2)
+IPv6=$(ifconfig "$IFACE" | awk '/inet6 /{print $2}' | cut -d' ' -f2)
 
 if [ ! -e "/opt/wgcf/wgcf-profile.conf" ]; then
-    IPv4=$(ifconfig "$IFACE" | awk '/inet /{print $2}' | cut -d' ' -f2)
-    IPv6=$(ifconfig "$IFACE" | awk '/inet6 /{print $2}' | cut -d' ' -f2)
+
     wgcf register --accept-tos && wgcf generate && mv wgcf-profile.conf /opt/wgcf
     sed -i "/\[Interface\]/a PostDown = ip -6 rule delete from ${IPv6}  lookup main" /opt/wgcf/wgcf-profile.conf
     sed -i "/\[Interface\]/a PostUp = ip -6 rule add from ${IPv6} lookup main" /opt/wgcf/wgcf-profile.conf
@@ -13,7 +14,7 @@ if [ ! -e "/opt/wgcf/wgcf-profile.conf" ]; then
     sed -i "/\[Interface\]/a PostUp = ip -4 rule add from ${IPv4} lookup main" /opt/wgcf/wgcf-profile.conf
 fi
 
-/bin/cp -rf /opt/wgcf/wgcf-profile.conf /etc/wireguard/warp.conf && /bin/cp -rf /opt/wgcf/danted.conf /etc/danted.conf
+/bin/cp -rf /opt/wgcf/wgcf-profile.conf /etc/wireguard/warp.conf
 wg-quick up warp
 
 exec "$@"
