f0f88382e407f62836b3f54c9d75523738942e97
WeYC <weovo@hotmail.com>
2024-01-05 19:32:58 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ IFACE=$(ip route show default | awk '{print $5}')
 if [ ! -e "/opt/wgcf/wgcf-profile.conf" ]; then
     IPv4=$(ifconfig "$IFACE" | awk '/inet /{print $2}' | cut -d' ' -f2)
     IPv6=$(ifconfig "$IFACE" | awk '/inet6 /{print $2}' | cut -d' ' -f2)
-    wgcf register --accept-tos && wgcf generate && mv wgcf-profile.conf /opt
+    wgcf register --accept-tos && wgcf generate && mv wgcf-profile.conf /opt/wgcf
     sed -i "/\[Interface\]/a PostDown = ip -6 rule delete from ${IPv6}  lookup main" /opt/wgcf/wgcf-profile.conf
     sed -i "/\[Interface\]/a PostUp = ip -6 rule add from ${IPv6} lookup main" /opt/wgcf/wgcf-profile.conf
     sed -i "/\[Interface\]/a PostDown = ip -4 rule delete from ${IPv4} lookup main" /opt/wgcf/wgcf-profile.conf
