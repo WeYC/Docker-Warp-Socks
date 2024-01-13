45cbf2edef09eda7891c1a54f5f62eaff9e33e70
WeYC <weovo@hotmail.com>
2024-01-13 15:34:54 +0800

    version: 0.4.4

--- download.sh
+++ download.sh
@@ -2,23 +2,20 @@
 
 set -e
 
-osCheck=$(uname -a)
-if [[ $osCheck =~ 'x86_64' ]]; then
-    architecture="amd64"
-elif [[ $osCheck =~ 'i386' ]]; then
-    architecture="386"
-elif [[ $osCheck =~ 'arm64' ]] || [[ $osCheck =~ 'aarch64' ]]; then
-    architecture="arm64"
-elif [[ $osCheck =~ 'armv7l' ]]; then
-    architecture="armv7"
-else
-    echo "暂不支持的系统架构，选择受支持的系统。"
-    exit 1
-fi
+ArchAffix() {
+    case "$(uname -m)" in
+    i386 | i686) echo '386' ;;
+    x86_64 | amd64) echo 'amd64' ;;
+    armv8 | arm64 | aarch64) echo 'arm64' ;;
+    arm*) echo "armv7" ;;
+    s390x) echo 's390x' ;;
+    *) echo "不支持的CPU架构!" && exit 1 ;;
+    esac
+}
 
 TAR="https://api.github.com/repos/XIU2/CloudflareSpeedTest/releases/latest"
 
-URL=$(curl -fsSL ${TAR} | grep 'browser_download_url' | cut -d'"' -f4 | grep linux | grep "$architecture")
+URL=$(curl -fsSL ${TAR} | grep 'browser_download_url' | cut -d'"' -f4 | grep linux | grep "$(ArchAffix)")
 
 echo "${URL}"
 
